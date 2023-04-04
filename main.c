cbc84542c9e7ecc8ee90045b8c3289eb78b458f4
babyoctopuses <babyoctopuses@gmail.com>
2023-04-04 12:22:08 +0700

    lesson-7-madlibs

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    double num1;
-    double num2;
-    printf("Enter first number: ");
-    scanf("%lf", &num1); //we use lf when we are scanning for a number
-    printf("Enter second number: ");
-    scanf("%lf", &num2);
-    printf("Your answer is %f ", num1+num2);
+
+    char color[20];
+    char pluralNoun[20];
+    char celebrityF[20];
+    char celebrityL[20]; 
+
+    printf("Enter a color: ");
+    scanf("%s", color); // & is not needed because we want to take a string input
+    printf("Enter a plural noun: ");
+    scanf("%s", pluralNoun); // & is not needed because we want to take a string input
+    printf("Enter a celebrity: ");
+    scanf("%s%s", celebrityF, celebrityL); // & is not needed because we want to take a string input
+    
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+    printf("I love %s %s\n", celebrityF, celebrityL);
+
+    return 0;
 }
