5ac7497e3474d01bf18686c16f61e63ca8f2c27c
Julfendi <babyoctopuses@gmail.com>
2023-04-04 14:37:13 +0700

    lesson-8-function

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
+void sayHi();
+
 int main(){
 
-    char color[20];
-    char pluralNoun[20];
-    char celebrityF[20];
-    char celebrityL[20]; 
-
-    printf("Enter a color: ");
-    scanf("%s", color); // & is not needed because we want to take a string input
-    printf("Enter a plural noun: ");
-    scanf("%s", pluralNoun); // & is not needed because we want to take a string input
-    printf("Enter a celebrity: ");
-    scanf("%s%s", celebrityF, celebrityL); // & is not needed because we want to take a string input
-    
-    printf("Roses are %s\n", color);
-    printf("%s are blue\n", pluralNoun);
-    printf("I love %s %s\n", celebrityF, celebrityL);
-
+    printf("Top \n");
+    sayHi("\nmichael", 30);
     return 0;
+
+}
+
+void sayHi(char name[], int age){
+    printf("Hello %s is %d years old", name, age);
 }
